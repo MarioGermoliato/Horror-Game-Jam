3e4e659e94379d4bc656fc1deca30c4ebff0270d
jaquesoler <jaquesoler@gmail.com>
2020-09-05 19:37:29 +0100

    Click Interact

---
+++ Assets/Scripts/ItemInteractedWith.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteractedWith : MonoBehaviour
+{
+    public bool isClicked;
+    //public Animator anim;
+
+    // Start is called before the first frame update
+    public void Interact()
+    {
+        if(!isClicked){
+            isClicked = true;
+            Debug.Log("Open");
+            //animator.SetBool("isActive", isClicked);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectInteract.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObjectInteract : MonoBehaviour
+{
+   public bool isInRange;
+   public KeyCode interactKey;
+   public UnityEvent interactAction;
+
+   void Update(){
+       if(isInRange){
+           if(Input.GetKeyDown(interactKey)){
+               interactAction.Invoke();
+           }
+       }
+   }
+
+   private void OnTriggerEnter2D(Collider2D collision){
+       if(collision.gameObject.CompareTag("Player")){
+           Debug.Log("meme");
+           isInRange = true;
+       }
+   }
+
+   private void OnTriggerExit2D(Collider2D collision){
+       if(collision.gameObject.CompareTag("Player")){
+           isInRange = false;
+       }
+   }
+}
