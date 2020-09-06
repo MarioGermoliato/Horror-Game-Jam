ac099f66433fde6744a52e3481c88fc9af99bed9
antoniocrorusso <56083560+antoniocrorusso@users.noreply.github.com>
2020-09-06 12:19:37 -0300

    Ghost Ai Follower

---
+++ Assets/Scripts/GhostFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostFollow : MonoBehaviour
+{
+    public float vel;
+    // Velocidade do movimento
+    public float distance;
+    // Distancia ate o player
+
+    private Transform target;
+
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
+    }
+
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > distance){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, vel * Time.deltaTime);
+        }
+    }
+}
