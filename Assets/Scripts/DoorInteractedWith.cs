fa12b2a4f2010b94d7856e446d5cbad29c77e4d0
jaquesoler <jaquesoler@gmail.com>
2020-09-08 17:10:13 +0100

    Transição de cena.

---
+++ Assets/Scripts/DoorInteractedWith.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorInteractedWith : MonoBehaviour
+{
+    public bool isClicked;
+    public string scene;
+    //public Animator anim;
+
+    // Start is called before the first frame update
+    public void Interact()
+    {
+        if(!isClicked){
+            isClicked = true;
+            Debug.Log("Open");
+            //animator.SetBool("isActive", isClicked);
+
+            if(this.tag == "Door"){
+                 SceneManager.LoadScene(scene);
+            }
+            else{
+
+            }
+
+        }
+    }
+}
