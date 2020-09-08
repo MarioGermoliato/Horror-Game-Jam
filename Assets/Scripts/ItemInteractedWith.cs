6420924fd76bfe88396f564f0d80f69e1e344ae6
jaquesoler <jaquesoler@gmail.com>
2020-09-08 16:22:29 +0100

    Eventos

--- Assets/Scripts/ItemInteractedWith.cs
+++ Assets/Scripts/ItemInteractedWith.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ItemInteractedWith : MonoBehaviour
 {
     public bool isClicked;
+    public GameObject dialogue;
     //public Animator anim;
 
     // Start is called before the first frame update
@@ -14,6 +15,15 @@ public class ItemInteractedWith : MonoBehaviour
             isClicked = true;
             Debug.Log("Open");
             //animator.SetBool("isActive", isClicked);
+
+            if(this.tag == "Npc"){
+                dialogue.SetActive(true);
+            }
+            else{
+
+            }
+
         }
     }
+
 }
